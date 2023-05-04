a026a25ccad8bb4d2bbbc2f96ca29bd59f744a4c
ShashirajAM <131737399+ShashirajAM@users.noreply.github.com>
2023-05-04 17:45:26 +0530

    Add files via upload

---
+++ age-ifelse.py
@@ -0,0 +1,10 @@
+age=int(input("age:")
+if age>10:
+    print("you are too old to my jokes")
+    print("why are you here")
+else:
+    print("here is the joke!!")
+number=int(input('number=:'))
+if (number=10 or number=50) and (number<100):
+    print('correct answer')
+
